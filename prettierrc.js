7f74c5a9f319e8dbfe66c1dd55e320e0bba2831f
Daniel Grant <danieljohngrant@gmail.com>
2018-08-10 18:12:39 +0100

    Add prettier precommit hook

---
+++ prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  semicolons: true,
+  singleQuote: false,
+  trailingComma: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: "always"
+};
